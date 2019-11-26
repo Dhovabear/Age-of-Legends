184b378389b6502e110f7476e96445111d2d32f1
Dhovabear <powerkiki25000@gmail.com>
2019-11-26 16:13:11 +0100

    Add(SampleScene): ajout de textures , de l'UI et autre
    -ajout des compteurs de mana et de cristaux -ajout de quelques textures pour les murs
    -ajout d'un format pour ecrire les gros chiffres
      (avec k pour 1000)

--- Assets/Scripts/CollectPhase/PlayerManager.cs
+++ Assets/Scripts/CollectPhase/PlayerManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
 namespace CollectPhase
@@ -10,10 +12,18 @@ namespace CollectPhase
 
         private List<PlayerData> _joueurs;
         private int _currentPlayer;
+        private Text cristIndic;
+        private Text manaIndic;
 
         public void Awake()
         {
             _joueurs = new List<PlayerData>();
+        }
+
+        public void Start()
+        {
+            cristIndic = GameObject.Find("cristauxLine").GetComponentInChildren<Text>();
+            manaIndic = GameObject.Find("manaLine").GetComponentInChildren<Text>();
         }
 
         // Start is called before the first frame update
@@ -26,6 +36,7 @@ namespace CollectPhase
             _currentPlayer = 0;
         }
 
+        
 
         public PlayerData GetCurrentPlayer()
         {
@@ -44,7 +55,15 @@ namespace CollectPhase
 
         private void Update()
         {
-            Debug.Log("cristaux: "+GetCurrentPlayer().cristaux + ";  Mana: "+GetCurrentPlayer().mana);
+            PlayerData pd = GetCurrentPlayer();
+            
+            Debug.Log("C: " + pd.cristaux + "   M: " + pd.mana);
+            
+            string cristTxt = (pd.cristaux >= 1000)?pd.cristaux/1000 + "," + (pd.cristaux % 1000) / 100 + "k" : pd.cristaux.ToString();
+            cristIndic.text = "x " + cristTxt;
+            
+            string manaTxt = (pd.mana >= 1000)?pd.mana/1000 + "," + (pd.mana % 1000) / 100 + "k" : pd.mana.ToString();
+            manaIndic.text = "x " + manaTxt;
         }
     }
 }
