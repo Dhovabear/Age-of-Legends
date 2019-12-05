a17eeb187c1532ae04e75fd8550bdcf8f38bc869
Anteck <antoine.varniern@gmail.com>
2019-12-05 15:13:37 +0100

    Debut cooldown

--- Assets/Scripts/Props/FarmBehavior.cs
+++ Assets/Scripts/Props/FarmBehavior.cs
@@ -6,18 +6,31 @@ public class FarmBehavior : MonoBehaviour
 {
 
     public GameObject pannelCreerPaysan;
+    public GameObject bouttonCreerPaysan;
     
     public GameObject paysan;
+
+    public float cooldown;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bouttonCreerPaysan.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer = (float) (timer - 0.01);
+        Debug.Log("Timer = "+timer);
+        if (timer <= 0)
+        {
+            timer = 0;
+            if (GameManager.current.GetPlayerManager().GetCurrentPlayer().mana > 200)
+            {
+                bouttonCreerPaysan.SetActive(true);
+            }
+        }
     }
 
     public void OnMouseOver()
@@ -25,6 +38,7 @@ public class FarmBehavior : MonoBehaviour
         if (Input.GetMouseButton(0))
         { 
             pannelCreerPaysan.SetActive(true);
+            bouttonCreerPaysan.SetActive(false);
         }
         
     }
@@ -43,8 +57,11 @@ public class FarmBehavior : MonoBehaviour
     {
         if (GameManager.current.GetPlayerManager().GetCurrentPlayer().mana > 200)
         {
+            
             paysan = GameObject.Instantiate(Resources.Load<GameObject>("Paysan"), gameObject.transform.position + Vector3.back*3, Quaternion.identity);
             GameManager.current.GetPlayerManager().GetCurrentPlayer().mana -= 200;
+            bouttonCreerPaysan.SetActive(false);
+            timer = 5;
         }
     }
 }
