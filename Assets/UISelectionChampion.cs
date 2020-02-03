c49e24ae5f96b96e3b5d04a8f9238fd07a92987a
VirgileMezz <37793334+VirgileMezz@users.noreply.github.com>
2020-02-03 20:31:53 +0100

    Menu de selection des personnages vers scene combat fonctionnel
    il faut encore que je passe les infos dans la scene de combat et tout seras bon

---
+++ Assets/UISelectionChampion.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISelectionChampion : MonoBehaviour
+{
+
+    private SelectionPersonnage sp;
+
+
+    void Start()
+    {
+        sp = GameObject.Find("SelectionPersonnage").GetComponent<SelectionPersonnage>();
+    }
+
+    // Update is called once per frame
+    
+    public void selectTeam1(int i)
+    {
+        sp.addToSelectedChampionTeam1(i);
+    }
+
+    public void selectTeam2(int i)
+    {
+        sp.addToSelectedChampionTeam2(i);
+
+    }
+
+    public void annulerTeam1()
+    {
+        sp.cancelSelectionTeam1();
+    }
+    public void annulerTeam2()
+    {
+        sp.cancelSelectionTeam2();
+
+    }
+
+
+    public void validerTeam1()
+    {
+        sp.validateTeam1();
+        sp.checkValidate();
+    }
+    public void validerTeam2()
+    {
+        sp.validateTeam2();
+        sp.checkValidate();
+
+    }
+}
