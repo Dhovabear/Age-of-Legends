503aa14a5980277a3f8d778ee6d0674e34af5517
Evan BLANC <evanblanc.pro@icloud.com>
2020-03-31 23:35:36 +0200

    In Game Menu

---
+++ Assets/_Scripts/In_Game_Menu_Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class In_Game_Menu_Script : MonoBehaviour {
+
+    private bool isMenuAsked;
+    private GameObject menu;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        menu = GameObject.Find("InGameMenu");
+        menu.SetActive(false);
+        isMenuAsked = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if ( Input.GetKeyDown("escape") ) {
+            if( ! isMenuAsked ) {
+                print("Open menu");
+                isMenuAsked = true;
+            }else {
+                print("Close menu");
+                isMenuAsked = false;
+            }
+
+            if ( isMenuAsked ) {    // load the menu 
+                menu.SetActive(true);
+            }else {     // hide the menu
+                menu.SetActive(false);
+            }
+        }
+    }
+}
