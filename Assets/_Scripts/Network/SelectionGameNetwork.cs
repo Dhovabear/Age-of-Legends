93c669cd0055217a6abb1168b81b2b7dbd07c651
VirgileMezz <37793334+VirgileMezz@users.noreply.github.com>
2020-03-31 10:43:02 +0200

    menu de sélection réseau

---
+++ Assets/_Scripts/Network/SelectionGameNetwork.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class SelectionGameNetwork : NetworkBehaviour
+{
+    private Text[] textSelectedTeam = new Text[5];
+    public GameObject[] selectedChampionTeam = new GameObject[5];
+    [SerializeField] private List<GameObject> champions = new List<GameObject>();
+
+
+
+    public GameObject testGo;
+    public GameObject[] gos = new GameObject[5];
+
+
+    [SyncVar]
+    public bool isValidate;
+    
+    private void Awake()
+    {
+        getAllChampions();
+
+    }
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        CmdChangeGo();
+        
+
+        CmdSetNameOfPlayer();
+        //CmdChangeChampion(0,0);
+        Debug.Log("end of start player");
+    }
+
+    private void Start()
+    {
+        if (!base.isServer)
+        {
+            if (isLocalPlayer)
+            {
+                gameObject.SetActive(true);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        
+    }
+    [Command]
+    private void CmdSetNameOfPlayer()
+    {
+        if (this.netId.ToString() == "2")
+        {
+            gameObject.name = "SelectionChampion1";
+
+        }
+        else if (this.netId.ToString() == "3")
+        {
+            gameObject.name = "SelectionChampion2";
+
+        }
+
+        //RpcSetIsFirst();
+    }
+
+
+    [Command]
+    private void CmdChangeGo()
+    {
+        GameObject go = gos[Random.Range(0, 4)];
+        testGo = go;
+        RpcChangeGo(go);
+
+    }
+
+    [ClientRpc]
+    private void RpcChangeGo(GameObject go)
+    {
+        Debug.Log(go);
+        testGo = go;
+    }
+
+    [Command]
+    private void CmdChangeChampion(int index, int i)
+    {
+        GameObject go = champions[index];
+        //selectedChampionTeam[i] = go;
+        Debug.Log("hello in the command");
+        setChampion(i, go);
+        //RpcChangeChampion(index, i);
+
+    }
+
+    [ClientRpc]
+    private void RpcChangeChampion(int index, int i)
+    {
+        GameObject go = champions[index];
+
+        //selectedChampionTeam[0] = go;
+        setChampion(i, go);
+    }
+
+    [Command]
+    private void CmdCancelChampion(int index)
+    {
+        textSelectedTeam[index].text = "";
+        selectedChampionTeam[index] = null;
+
+    }
+
+    [Command]
+    private void CmdSetIsValidate(bool b)
+    {
+        isValidate = b;
+    }
+
+    private void Update()
+    {
+
+        if (!base.isLocalPlayer)
+        {
+
+            return;
+
+        }
+    }
+
+    private void getAllChampions()
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            textSelectedTeam[i - 1] = gameObject.transform.Find("TeamCanvas/SelectedChampion" + i + "/Text").GetComponent<Text>();
+
+        }
+        Object[] prefabs = Resources.LoadAll("Champions"); //(GameObject[])
+        foreach (Object go in prefabs)
+        {
+            champions.Add(go as GameObject);
+        }
+        Debug.Log("getting champion");
+
+    }
+
+    public void selectChampion(int index)
+    {
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (selectedChampionTeam[i] == null && !checkSelectedChampion(champions[index]))
+            {
+                Debug.Log("selection loop");
+                selectedChampionTeam[i] = champions[index];
+                textSelectedTeam[i].text = selectedChampionTeam[i].name;
+                //setChampion(i, champions[index]);
+                CmdChangeChampion(index, i);
+
+                break;
+
+
+            }
+        }
+
+    }
+    private bool checkSelectedChampion(GameObject go)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (selectedChampionTeam[i] != null && selectedChampionTeam[i].name == go.name)
+            {
+                Debug.Log("check " + i + " times");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void cancelSelectionTeam()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            textSelectedTeam[i].text = "";
+            selectedChampionTeam[i] = null;
+            CmdCancelChampion(i);
+
+        }
+        isValidate = false;
+        CmdSetIsValidate(false);
+    }
+
+
+    public void validateTeam()
+    {
+
+        if (selectedChampionTeam[4] != null)
+        {
+            isValidate = true;
+            CmdSetIsValidate(true);
+
+        }
+
+    }
+
+    public void setChampion(int index, GameObject go)
+    {
+        selectedChampionTeam[index] = go;
+        Debug.Log("set champion text : "+ textSelectedTeam[index]);
+        Debug.Log("set champion team : " + selectedChampionTeam[index]+" champioooooooooooooooooooooooon : "+go);
+
+        textSelectedTeam[index].text = selectedChampionTeam[index].name;
+    }
+}
---
+++ Assets/_Scripts/Network/TeamsControllerNetwork.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+public class TeamsControllerNetwork : NetworkBehaviour
+{
+    public GameObject[] selectedChampionTeam1 = new GameObject[5];
+    public GameObject[] selectedChampionTeam2 = new GameObject[5];
+
+    public SelectionGameNetwork player1sg;
+    public SelectionGameNetwork player2sg;
+
+    public Text[] texts = new Text[10];
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("net id team controller : " + this.netId);
+        for(int i = 1; i < 11; i++)
+        {
+            texts[i - 1] = transform.Find("Canvas/TextChampion" + i).GetComponent<Text>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (!isLocalPlayer)
+        {
+            return;
+        }*/
+        for(int i = 0; i < 5; i++)
+        {
+            if(selectedChampionTeam1[i] != null)
+            {
+                texts[i].text = selectedChampionTeam1[i].name;
+            }
+            else
+            {
+                texts[i].text = "";
+            }
+        }
+        for (int i = 5; i < 10; i++)
+        {
+            if (selectedChampionTeam2[i-5] != null)
+            {
+                texts[i].text = selectedChampionTeam2[i - 5].name;
+
+            }
+            else
+            {
+                texts[i].text = "";
+            }
+
+        }
+        //CmdPrintChampion();
+        if(player1sg == null && GameObject.Find("SelectionChampion1") !=null)
+        {
+            player1sg = GameObject.Find("SelectionChampion1").GetComponent<SelectionGameNetwork>();
+        }
+        if (player2sg == null && GameObject.Find("SelectionChampion2") != null)
+        {
+            player2sg = GameObject.Find("SelectionChampion2").GetComponent<SelectionGameNetwork>();
+
+        }
+        if (player1sg != null && player2sg != null)
+        {
+            selectedChampionTeam1 = player1sg.selectedChampionTeam;
+            selectedChampionTeam2 = player2sg.selectedChampionTeam;
+            if(player1sg.isValidate && player2sg.isValidate)
+            {
+                //CHANGE SCENE
+                Debug.Log("changer scene");
+                NetworkManager.singleton.ServerChangeScene("CollectPhaseScene");
+
+
+            }
+        }
+ 
+
+    }
+
+
+    [ClientRpc]
+    public void RpcSetSelectedChampions()
+    {
+        CmdSetSelectedChampions();
+    }
+
+    [Command]
+    public void CmdSetSelectedChampions()
+    {
+        selectedChampionTeam1 = player1sg.selectedChampionTeam;
+        selectedChampionTeam2 = player2sg.selectedChampionTeam;
+    }
+
+
+    [Command]
+    public void CmdPrintChampion()
+    {
+        Debug.Log("first of list 1 : " + selectedChampionTeam1[0]);
+        Debug.Log("first of list 2 : " + selectedChampionTeam2[0]);
+    }
+}
